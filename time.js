553f9b254124cde83fdda27656249647df969e01
GachiBusik <98333255+GachiBusik@users.noreply.github.com>
2023-01-25 08:16:06 +0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,18 @@
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var clock = document.getElementById("clock");
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var currentTimeString = hours + ":" + minutes + ":" + seconds;
+    clock.innerHTML = currentTimeString;
+}
